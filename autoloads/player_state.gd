54079c863ad05337606492ff19b0454b60c874a9
Mandy Choi <hello@mandychoi.com.au>
2025-05-13 00:47:22 +0100

    Increase chaos when player works

--- autoloads/player_state.gd
+++ autoloads/player_state.gd
@@ -1,9 +1,15 @@
 extends Node
 
 var money: int = 0
+var chaos: int = 0
 
 signal money_changed(amount)
+signal chaos_changed(amount)
 
 func earn_money(amount: int) -> void:
 	money += amount
 	money_changed.emit(money)
+
+func increase_chaos(amount: int) -> void:
+	chaos += amount
+	chaos_changed.emit(chaos)
--- scenes/game_ui.gd
+++ scenes/game_ui.gd
@@ -2,10 +2,17 @@ extends Node
 
 @onready var work_button := $ActionButtons/WorkButton as ActionButton
 @onready var money_label := $StatusBar/MoneyLabel as MoneyLabel
+@onready var chaos_bar := $StatusBar/ChaosBar
 
 func _ready():
 	work_button.pressed.connect(_on_work_button_pressed)	
 	PlayerState.money_changed.connect(money_label.update_amount)
+	PlayerState.chaos_changed.connect(_update_chaos_level)
 
 func _on_work_button_pressed():
+#	// TODO depends on the job
 	PlayerState.earn_money(10)
+	PlayerState.increase_chaos(5)
+
+func _update_chaos_level(new_amount: int):
+	chaos_bar.value = new_amount
