aeecb0012d9baba6169dc42344bbda45cce23e50
Mandy Choi <hello@mandychoi.com.au>
2025-05-13 00:37:59 +0100

    Update money label when player earns money

--- scenes/game_ui.gd
+++ scenes/game_ui.gd
@@ -1,9 +1,11 @@
 extends Node
 
-@onready var work_button := $ActionPanel/ActionButtons/WorkButtonContainer/WorkButton as ActionButton
+@onready var work_button := $ActionButtons/WorkButton as ActionButton
+@onready var money_label := $StatusBar/MoneyLabel as MoneyLabel
 
 func _ready():
-	work_button.pressed.connect(_on_work_button_pressed)
+	work_button.pressed.connect(_on_work_button_pressed)	
+	PlayerState.money_changed.connect(money_label.update_amount)
 
 func _on_work_button_pressed():
 	PlayerState.earn_money(10)
---
+++ scripts/ui/money_label.gd
@@ -0,0 +1,16 @@
+class_name MoneyLabel
+extends Label
+
+@export var prefix := "$"
+@export var animate_scale := true
+
+@onready var initial_scale := scale
+
+func update_amount(new_amount: int):
+	text = prefix + str(new_amount)
+	_play_scale_animation()
+
+func _play_scale_animation():
+	var tween = create_tween().set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(self, "scale", initial_scale * 1.2, 0.15)
+	tween.tween_property(self, "scale", initial_scale, 0.35)
