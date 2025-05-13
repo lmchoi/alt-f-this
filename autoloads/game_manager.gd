69e3778bb61d14688be6fd7363de7d6db921901b
Mandy Choi <hello@mandychoi.com.au>
2025-05-13 21:54:24 +0100

    Show event on random when work is clicked

--- autoloads/game_manager.gd
+++ autoloads/game_manager.gd
@@ -1,15 +1,33 @@
 extends Node
-
-var money: int = 0
-var chaos: int = 0
-
 signal money_changed(amount)
 signal chaos_changed(amount)
+signal event_occurred(event_data)
 
-func earn_money(amount: int) -> void:
-	money += amount
-	money_changed.emit(money)
+var money := 0:
+	set(value):
+		money = value
+		money_changed.emit(money)
 
-func increase_chaos(amount: int) -> void:
-	chaos += amount
-	chaos_changed.emit(chaos)
+var chaos := 0:
+	set(value):
+		chaos = value
+		chaos_changed.emit(chaos)
+
+const WORK_EVENTS := [
+	{"text": "Boss says: 'We’re a family.'", "chaos": +20, "money": 0},
+	{"text": "Free pizza! (It's vegan.)", "chaos": -5, "money": 0},
+	{"text": "Legacy code explodes. Debug for 3 hours.", "chaos": +25, "money": -50}
+]
+
+func do_work():
+	money += 10
+	chaos += 5
+
+	var event_result := {"text": "", "money": 0, "chaos": 0}
+
+	if randf() <= 0.3:  # 30% chance for event
+		event_result = WORK_EVENTS[randi() % WORK_EVENTS.size()]
+		money += event_result.money
+		chaos += event_result.chaos
+
+	event_occurred.emit(event_result)
--- scenes/game_ui.gd
+++ scenes/game_ui.gd
@@ -10,17 +10,20 @@ func _ready():
 	slack_button.pressed.connect(_on_slack_button_pressed)
 	GameManager.money_changed.connect(money_label.update_amount)
 	GameManager.chaos_changed.connect(_update_chaos_level)
+	GameManager.event_occurred.connect(_on_event_occurred)
 
 func _on_work_button_pressed():
-#	// TODO depends on the job
-	GameManager.earn_money(10)
-	GameManager.increase_chaos(5)
+	GameManager.do_work()
 
 func _on_slack_button_pressed():
 	# TODO more generic function to update chaos
 	# TODO can't go below 0
 	GameManager.increase_chaos(-10)
 	$EventPopup.show_event("Slacker")
-
+	
 func _update_chaos_level(new_amount: int):
 	chaos_bar.value = new_amount
+
+func _on_event_occurred(event: Dictionary):
+	if event.text != "":
+		$EventPopup.show_event(event.text)
