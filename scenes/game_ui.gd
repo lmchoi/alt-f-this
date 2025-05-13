73c3655575ce528c4eaac16be0a0efa56dc9cb9a
Mandy Choi <hello@mandychoi.com.au>
2025-05-13 20:47:37 +0100

    Implement slack button to decrease chaos

--- scenes/game_ui.gd
+++ scenes/game_ui.gd
@@ -1,11 +1,13 @@
 extends Node
 
 @onready var work_button := $ActionButtons/WorkButton as ActionButton
+@onready var slack_button := $ActionButtons/SlackButton as ActionButton
 @onready var money_label := $StatusBar/MoneyLabel as MoneyLabel
 @onready var chaos_bar := $StatusBar/ChaosBar
 
 func _ready():
-	work_button.pressed.connect(_on_work_button_pressed)	
+	work_button.pressed.connect(_on_work_button_pressed)
+	slack_button.pressed.connect(_on_slack_button_pressed)
 	PlayerState.money_changed.connect(money_label.update_amount)
 	PlayerState.chaos_changed.connect(_update_chaos_level)
 
@@ -13,6 +15,11 @@ func _on_work_button_pressed():
 #	// TODO depends on the job
 	PlayerState.earn_money(10)
 	PlayerState.increase_chaos(5)
+
+func _on_slack_button_pressed():
+	# TODO more generic function to update chaos
+	# TODO can't go below 0
+	PlayerState.increase_chaos(-10)
 
 func _update_chaos_level(new_amount: int):
 	chaos_bar.value = new_amount
