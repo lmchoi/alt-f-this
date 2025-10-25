d7b0c0cff3cb79981e5c1a908c403c9e1156a0f8
mandy <hello@mandychoi.com.au>
2025-10-25 12:41:15 +0100

    Update deadline dialog to make it clearer what each option means

--- scenes/dialogs/deadline_dialog.gd
+++ scenes/dialogs/deadline_dialog.gd
@@ -2,11 +2,11 @@ extends AcceptDialog
 
 
 func _ready():
-	title = "Deadline Police!"
-	dialog_text = "THE DEADLINE POLICE ARE HERE! What will you do?"
-	
+	title = "Deadline: Today"
+	dialog_text = "Task is due today.\n\nYou're out of time.\n\nShip it now or negotiate an extension.\n(3 days overdue = fired)"
+
 	get_ok_button().hide()
-	
+
 	# Add custom buttons (text, right-aligned, action_id)
-	add_button("Beg for Mercy", false, "mercy")  
-	add_button("Duck It!", false, "duck_it")
+	add_button("Ask for Extension", false, "mercy")
+	add_button("Ship It Now", false, "duck_it")
