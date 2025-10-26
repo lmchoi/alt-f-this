5e187b07282be2e5e7e94d2af665efdf8106cf9a
mandy <hello@mandychoi.com.au>
2025-10-26 07:31:11 +0000

    Move bugs from top bar to job info

--- scenes/job_info.gd
+++ scenes/job_info.gd
@@ -1,13 +1,18 @@
 extends PanelContainer
 
 @onready var salary_label := $"%SalaryLabel"
+@onready var bugs_value := $"%BugsValue"
 
 func _ready():
+	GameManager.bugs_changed.connect(_update_bugs)
 	GameManager.next_day.connect(_update_payday)
 	GameManager.payday_occurred.connect(_on_payday)
 
-	# Initialize with current value
+	_update_bugs(GameManager.bugs)
 	_update_payday(GameManager.day)
+
+func _update_bugs(amount: int):
+	bugs_value.text = str(amount)
 
 func _update_payday(_day: int):
 	var days = GameManager.days_until_payday - 1
